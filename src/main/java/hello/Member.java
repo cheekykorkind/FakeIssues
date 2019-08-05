cb4e832ba063c23a420a0451191dd5ab46fdd11f
cheekykorkind <cheekorkind@gmail.com>
2019-08-06 00:07:32 +0900

    login user의id를 취득하기위해 SecurityMember클래스 조정

--- src/main/java/hello/Member.java
+++ src/main/java/hello/Member.java
@@ -1,5 +1,5 @@
 package hello;
- 
+
 import java.util.Collection;
 
 import org.springframework.security.core.GrantedAuthority;
@@ -9,17 +9,17 @@ public class Member implements UserDetails {
     private Long id;
 
     private String username;
-        
+
     private String password;
-    
+
     private String name;
-    
+
     private boolean isAccountNonExpired;
-    
+
     private boolean isAccountNonLocked;
-    
+
     private boolean isCredentialsNonExpired;
-    
+
     private boolean isEnabled;
 
     private Collection<? extends GrantedAuthority> authorities;
@@ -27,63 +27,63 @@ public class Member implements UserDetails {
     public Long getId() {
         return this.id;
     }
-    
+
     public void setId(Long id) {
         this.id = id;
     }
-    
+
     public String getUsername() {
         return username;
     }
-    
+
     public void setUsername(String username) {
         this.username = username;
     }
-    
+
     public String getPassword() {
         return password;
     }
-    
+
     public void setPassword(String password) {
         this.password = password;
     }
-    
+
     public String getName() {
         return name;
     }
-    
+
     public void setName(String name) {
         this.name = name;
     }
-    
+
     public boolean isAccountNonExpired() {
         return isAccountNonExpired;
     }
-    
+
     public void setAccountNonExpired(boolean isAccountNonExpired) {
         this.isAccountNonExpired = isAccountNonExpired;
     }
-    
+
     public boolean isAccountNonLocked() {
         return isAccountNonLocked;
     }
-    
+
     public void setAccountNonLocked(boolean isAccountNonLocked) {
         this.isAccountNonLocked = isAccountNonLocked;
     }
-    
+
     public boolean isCredentialsNonExpired() {
         return isCredentialsNonExpired;
     }
-    
+
     public void setCredentialsNonExpired(boolean isCredentialsNonExpired) {
         this.isCredentialsNonExpired = isCredentialsNonExpired;
     }
-    
+
     public boolean isEnabled() {
         return isEnabled;
     }
-    
+
     public void setEnabled(boolean isEnabled) {
         this.isEnabled = isEnabled;
     }
@@ -91,7 +91,7 @@ public class Member implements UserDetails {
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return authorities;
     }
- 
+
     public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
         this.authorities = authorities;
     }
