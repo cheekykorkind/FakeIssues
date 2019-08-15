0a121d337ab3bc0bb72d0018d7a51754803b9b33
cheekykorkind <cheekorkind@gmail.com>
2019-08-15 02:54:23 -0400

    add mysql container, use docker-compose-dev.yml

--- server/docker/docker_compose_up.sh
+++ server/docker/docker_compose_up.sh
@@ -11,29 +11,39 @@ if [ $1 != "up" ] && [ $1 != "down" ]; then
     exit 1
 fi
 
+### Gradle(workspace) #################################################
 UID=$(id -u $USER)
 GID=$(id -g $USER)
 UNAME=$(echo "$USER")
-MY_DOCKER_IMAGE_NAME="fake_issue"
+
+WORKSPACE_TIMEZONE=UTC
+### Gradle #################################################
+
+### MYSQL #################################################
+MYSQL_VERSION=5.7
+MYSQL_DATABASE=docker_fake_issue_dev
+MYSQL_USER=root
+MYSQL_PASSWORD=root
+MYSQL_PORT=3306
+MYSQL_ROOT_PASSWORD=root
+### MYSQL #################################################
 
 cp docker-compose-dev.yml docker-compose.yml
 
 sed -i "s/\(UID=70000\)/UID=${UID}/" docker-compose.yml
 sed -i "s/\(GID=70000\)/GID=${GID}/" docker-compose.yml
 sed -i "s/\(vagrant1\)/${UNAME}/" docker-compose.yml
-sed -i "s/\(dynamic_uid_image\)/${MY_DOCKER_IMAGE_NAME}/" docker-compose.yml
+sed -i "s/\(\${WORKSPACE_TIMEZONE}\)/${WORKSPACE_TIMEZONE}/" docker-compose.yml
 
-echo "${GREEN}docker-compose.yml created${NC}"
 
-if [ $1 = "up" ]; then
-    docker-compose build --no-cache ${MY_DOCKER_IMAGE_NAME} \
-    && echo "${GREEN}Additional step : docker-compose build --no-cache ${MY_DOCKER_IMAGE_NAME} ${NC}" \
-    && docker-compose up -d \
-    && echo "${GREEN}Additional step : docker-compose up -d ${NC}" \
-    && docker exec ${MY_DOCKER_IMAGE_NAME} chown -R ${UNAME}:${UNAME} /home/${UNAME}/opt \
-    && echo "${GREEN}Additional step : docker exec ${MY_DOCKER_IMAGE_NAME} chown -R ${UNAME}:${UNAME} /home/${UNAME}/opt ${NC}" \
-    && echo "${GREEN}Additional finish${NC}"
-else
-    docker-compose down && echo "${GREEN}docker-compose down${NC}"
-    rm docker-compose.yml && echo "${GREEN}docker-compose.yml removed${NC}"
-fi
+sed -i "s/\(\${MYSQL_VERSION}\)/${MYSQL_VERSION}/" docker-compose.yml
+sed -i "s/\(\${MYSQL_DATABASE}\)/${MYSQL_DATABASE}/" docker-compose.yml
+sed -i "s/\(\${MYSQL_USER}\)/${MYSQL_USER}/" docker-compose.yml
+sed -i "s/\(\${MYSQL_PASSWORD}\)/${MYSQL_PASSWORD}/" docker-compose.yml
+sed -i "s/\(\${MYSQL_ROOT_PASSWORD}\)/${MYSQL_ROOT_PASSWORD}/" docker-compose.yml
+sed -i "s/\(\${MYSQL_PORT}\)/${MYSQL_PORT}/" docker-compose.yml
+
+echo "${GREEN}start to create docker-compose.yml${NC}"
+
+echo "${GREEN}Set volumn directory permission by yourself. Type below${NC}"
+echo "${GREEN}Type this : docker exec MY_DOCKER_IMAGE_NAME chown -R ${UNAME}:${UNAME} /home/${UNAME}/opt ${NC}"
